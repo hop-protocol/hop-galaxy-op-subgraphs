b7ac1483a34ef773033101afb4556bc25dbe4fce
Miguel Mota <hello@miguelmota.com>
2022-09-16 17:42:09 -0700

    Read initial balance

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,28 +6,44 @@ export function updateBigIntDecimals(amount: BigInt, decimals: number): BigInt {
   return amount.div(BigInt.fromI64(10).pow(decimals as u8))
 }
 
+export function getLpBalance(contractAddress: string, account: Address): BigInt {
+  const contract = ERC20.bind(Address.fromString(contractAddress))
+  const balanceCallResult = contract.try_balanceOf(account)
+  const decimalsCallResult = contract.try_decimals()
+  const decimals = decimalsCallResult.value
+  const balance = updateBigIntDecimals(balanceCallResult.value, decimals)
+  return balance
+}
+
 export function handleTransfer(event: Transfer): void {
   const blockTimestamp = event.params._event.block.timestamp
-  const fromAddress = event.params.from.toHexString()
-  const toAddress = event.params.to.toHexString()
+  const fromAddress = event.params.from
+  const toAddress = event.params.to
 
   const tokenAddress = event.address
   const contract = ERC20.bind(tokenAddress)
-  let callResult = contract.try_decimals()
-  let decimals = callResult.value
+  const callResult = contract.try_decimals()
+  const decimals = callResult.value
   let transferAmount = updateBigIntDecimals(event.params.value, decimals)
 
-  const isEth = tokenAddress.equals(Address.fromHexString('0x5C2048094bAaDe483D0b1DA85c3Da6200A88a849'))
+  const isEth = tokenAddress.equals(Address.fromString('0x5C2048094bAaDe483D0b1DA85c3Da6200A88a849'))
   if (isEth) {
    const rate = BigInt.fromI64(36500).div(BigInt.fromI64(24))
    transferAmount = rate.times(transferAmount)
   }
 
   {
-    let id = fromAddress
+    const id = fromAddress.toHexString()
     let entity = AccountEntity.load(id)
     if (entity == null) {
       entity = new AccountEntity(id)
+
+      const usdcLpBalance = getLpBalance('0x2e17b8193566345a2Dd467183526dEdc42d2d5A8', fromAddress)
+      const usdtLpBalance = getLpBalance('0xF753A50fc755c6622BBCAa0f59F0522f264F006e', fromAddress)
+      const daiLpBalance = getLpBalance('0x22D63A26c730d49e5Eab461E4f5De1D8BdF89C92', fromAddress)
+      const ethLpBalance = getLpBalance('0x5C2048094bAaDe483D0b1DA85c3Da6200A88a849', fromAddress)
+      const initialBalance = usdcLpBalance.plus(usdtLpBalance).plus(daiLpBalance).plus(ethLpBalance)
+      entity.totalBalance = initialBalance
     }
 
     let totalBalance = entity.totalBalance
@@ -37,7 +53,7 @@ export function handleTransfer(event: Transfer): void {
     totalBalance = totalBalance.minus(transferAmount)
     tokenDays = tokenDays.plus((blockTimestamp.minus(lastUpdated)).times(totalBalance))
 
-    entity.account = fromAddress
+    entity.account = fromAddress.toHexString()
     entity.lastUpdated = blockTimestamp
     entity.totalBalance = totalBalance
 
@@ -45,10 +61,17 @@ export function handleTransfer(event: Transfer): void {
   }
 
   {
-    let id = toAddress
+    const id = toAddress.toHexString()
     let entity = AccountEntity.load(id)
     if (entity == null) {
       entity = new AccountEntity(id)
+
+      const usdcLpBalance = getLpBalance('0x2e17b8193566345a2Dd467183526dEdc42d2d5A8', toAddress)
+      const usdtLpBalance = getLpBalance('0xF753A50fc755c6622BBCAa0f59F0522f264F006e', toAddress)
+      const daiLpBalance = getLpBalance('0x22D63A26c730d49e5Eab461E4f5De1D8BdF89C92', toAddress)
+      const ethLpBalance = getLpBalance('0x5C2048094bAaDe483D0b1DA85c3Da6200A88a849', toAddress)
+      const initialBalance = usdcLpBalance.plus(usdtLpBalance).plus(daiLpBalance).plus(ethLpBalance)
+      entity.totalBalance = initialBalance
     }
 
     let totalBalance = entity.totalBalance
@@ -58,7 +81,7 @@ export function handleTransfer(event: Transfer): void {
     totalBalance = totalBalance.plus(transferAmount)
     tokenDays = tokenDays.plus((blockTimestamp.minus(lastUpdated)).times(totalBalance))
 
-    entity.account = toAddress
+    entity.account = toAddress.toHexString()
     entity.lastUpdated = blockTimestamp
     entity.totalBalance = totalBalance
 
